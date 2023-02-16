f14bafc1d4ed2725050736cb18c3143e1ce502a3
Alan <ansarialan31@gmail.com>
2023-02-16 13:47:50 +0530

    ADD: email and password validations

--- src/components/input/Email.jsx
+++ src/components/input/Email.jsx
@@ -1,24 +1,47 @@
 import React, { useState } from 'react'
-import { TextField } from '@mui/material';
+import { TextField, Tooltip } from '@mui/material';
 
 
 const Email = () => {
 
   const [val,setVal] = useState('');
+  const [isValid,setValid] = useState(true);
 
   function handleChange(e) {
-    setVal(e.target.value.trim())
+    let value = e.target.value;
+    setVal(value.trim())
+    if(value!=='')
+    setValid(value.match(
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    ));
   }
 
+  let content;
+
+  (isValid)?
+    content = <Tooltip title='' placement='right'><TextField
+    required
+    id="outlined-basic"
+    variant="outlined"
+    fullWidth
+    label='Email'
+    value={val}
+    onChange={(e)=>{handleChange(e)}}
+    /></Tooltip>
+  : content = <Tooltip arrow title="Invalid Email" placement="right"><TextField
+      error
+      required
+      id="outlined-error-helper-text"
+      label="Email"
+      value={val}
+      fullWidth
+      onChange={(e)=>{handleChange(e)}}
+/></Tooltip>;
+
   return (
-      <TextField
-       id="outlined-basic"
-       variant="outlined"
-       fullWidth
-       label='Email *'
-       value={val}
-       onChange={(e)=>{handleChange(e)}}
-       />
+      <>
+        {content}
+      </>
   )
 }
 
--- src/components/input/Password.jsx
+++ src/components/input/Password.jsx
@@ -1,15 +1,58 @@
-import React from 'react'
-import { TextField } from '@mui/material';
+import {React,useState} from 'react'
+import { styled } from '@mui/material/styles';
+import { TextField, Tooltip, tooltipClasses} from '@mui/material';
+
+const CustomWidthTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))({
+  [`& .${tooltipClasses.tooltip}`]: {
+    maxWidth: 200,
+  },
+});
 
 const Password = () => {
+
+  const [val,setVal] = useState('');
+  const [isValid,setValid] = useState(true);
+
+  function handleChange(e) {
+    let value = e.target.value;
+    setVal(value.trim())
+    if(value!=='')
+    setValid(value.match(
+      /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+    ));
+  }
+
+  let content;
+
+  (isValid)?
+    content = <CustomWidthTooltip title='' placement='right'><TextField
+    required
+    id="outlined-password-input"
+    label="Password"
+    type="password"
+    autoComplete="current-password"
+    fullWidth
+    value={val}
+    onChange={(e)=>{handleChange(e)}}
+    /></CustomWidthTooltip>
+  : content = <CustomWidthTooltip arrow title="Password must contain atleast 8 characters including one uppercase, one lowercase, one number and a special character"
+       placement="right"><TextField
+      error
+      required
+      id="outlined-password-input"
+      type="password"
+      label="Password"
+      value={val}
+      fullWidth
+      onChange={(e)=>{handleChange(e)}}
+      /></CustomWidthTooltip>;
+
     return (
-        <TextField
-          id="outlined-password-input"
-          label="Password *"
-          type="password"
-          fullWidth='true'
-          autoComplete="current-password"
-        />
+        <>
+        {content}
+        </>
     )
 }
 
